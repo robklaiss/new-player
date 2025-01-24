64f58240305f6f25e44b4ecf021a59adb76f0df8
Robin Klaiss <robin@vinculo.com.py>
2025-01-24 19:35:45 -0300

    Add kiosk startup script

---
+++ raspberry-files/kiosk.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Disable screen saver and power management
+xset s noblank
+xset s off
+xset -dpms
+
+# Hide the cursor when inactive
+unclutter -idle 0.5 -root &
+
+# Fix for Chromium crash issues
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
+
+# Start Chromium in kiosk mode
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://localhost/index.html \
+    --disable-translate \
+    --disable-features=TranslateUI \
+    --disable-sync \
+    --disable-save-password-bubble \
+    --disable-session-crashed-bubble \
+    --disable-component-update \
+    --disable-features=PreloadMediaEngagementData,AutoplayIgnoreWebAudio,MediaEngagementBypassAutoplayPolicies
