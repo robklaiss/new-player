f0d5566afe434288ca7e32b14cf27d5c19d58a6e
Robin Klaiss <robin@vinculo.com.py>
2025-01-25 01:19:15 -0300

    changes

--- server-files/api/content.php
+++ server-files/api/content.php
@@ -2,30 +2,27 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-// Get the latest video from the videos directory
+// Get all videos from the videos directory
 $videos_dir = __DIR__ . '/../videos/';
 $videos = glob($videos_dir . '*.mp4');
+$video_list = [];
 
-if (empty($videos)) {
-    echo json_encode([
-        'error' => 'No videos available'
-    ]);
-    exit;
+foreach ($videos as $video) {
+    $video_list[] = [
+        'url' => 'https://vinculo.com.py/new-player/videos/' . basename($video),
+        'filename' => basename($video),
+        'modified' => filemtime($video)
+    ];
 }
 
-// Get the most recently modified video
-$latest_video = array_reduce($videos, function($latest, $video) {
-    if (!$latest || filemtime($video) > filemtime($latest)) {
-        return $video;
-    }
-    return $latest;
+// Sort by modified time, newest first
+usort($video_list, function($a, $b) {
+    return $b['modified'] - $a['modified'];
 });
-
-$video_url = 'https://vinculo.com.py/new-player/videos/' . basename($latest_video);
 
 echo json_encode([
     'version' => time(),
     'content' => [
-        'video' => $video_url
+        'videos' => $video_list
     ]
 ]);
