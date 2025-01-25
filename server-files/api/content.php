9dfd8d776c28927a5e71ca83d4e571d06d115257
Robin Klaiss <robin@vinculo.com.py>
2025-01-25 01:25:43 -0300

    changes

--- server-files/api/content.php
+++ server-files/api/content.php
@@ -2,10 +2,22 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
+// Enable error reporting for debugging
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 // Get all videos from the videos directory
 $videos_dir = __DIR__ . '/../videos/';
 $videos = glob($videos_dir . '*.mp4');
 $video_list = [];
+
+// Debug info
+$debug = [
+    'directory' => $videos_dir,
+    'exists' => is_dir($videos_dir),
+    'readable' => is_readable($videos_dir),
+    'files_found' => count($videos)
+];
 
 foreach ($videos as $video) {
     $video_list[] = [
@@ -24,5 +36,6 @@ echo json_encode([
     'version' => time(),
     'content' => [
         'videos' => $video_list
-    ]
+    ],
+    'debug' => $debug
 ]);
